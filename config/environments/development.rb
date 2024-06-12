0cd8c442f0905864c26e9d69cfa382bc15d29970
fahadiakhalda <khalda.nisa23@gmail.com>
2024-06-12 07:42:41 +0000

    Membuat Kolom Article

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "musical-umbrella-q764w7rj529j7r-3000.app.github.dev"
+  config.hosts << "fuzzy-happiness-q797g44jp4j734ggv-3000.app.github.dev"
 end
